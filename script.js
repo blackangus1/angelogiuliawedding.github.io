f5e8856023a83da5de9e73c59d21e60b2f9a840c
Roberto Carcamo <r.carcamo@be-tse.it>
2023-04-26 15:16:19 +0200

    fix mobile and form

--- script.js
+++ script.js
@@ -2,59 +2,123 @@ const links = document.querySelectorAll(".navbar > nav > ul > li");
 const cards = document.querySelectorAll(".card");
 
 [...links].map((link, index) => {
-  link.addEventListener("click", () => onLinkClick(link, index), false);
+    link.addEventListener("click", () => onLinkClick(link, index), false);
 });
 
 const onLinkClick = (link, currentIndex) => {
-  const selectedItem = link.getAttribute("name");
-  cards.forEach((card) => {
-    card.classList.remove("active");
-  });
-  // const currentCard = [...cards].find((card) =>
-  //   card.classList.contains(selectedItem)
-  // );
+    const selectedItem = link.getAttribute("name");
+    cards.forEach((card) => {
+        card.classList.remove("active");
+    });
 
-  const currentCard = [...cards].find((card) =>
-    Object.keys(card.dataset).includes(selectedItem)
-  );
-  currentCard.classList.add("active");
-  highLightSelectedLink(currentIndex);
+    const currentCard = [...cards].find((card) =>
+        Object.keys(card.dataset).includes(selectedItem)
+    );
+    currentCard.classList.add("active");
+    highLightSelectedLink(currentIndex);
+
+    if (jQuery('aside').hasClass('opened')) {
+        toggleSidebar();
+    }
 };
 
 const highLightSelectedLink = (currentIndex) => {
-  links.forEach((link) => {
-    link.classList.remove("selectedLink");
-  });
-  links[currentIndex].classList.add("selectedLink");
+    links.forEach((link) => {
+        link.classList.remove("selectedLink");
+    });
+    links[currentIndex].classList.add("selectedLink");
 };
-/*Submit*/
-jQuery("#rsvpForm").submit(function(e){
-  e.preventDefault();
+
+/**
+ * Welcome slide images
+ */
+
+const welcome_section = jQuery('.welcome');
+if (welcome_section.length) {
+    const images = jQuery(welcome_section[0]).find('.image');
+    let activeImage = 0;
+    jQuery(images[activeImage]).fadeIn(500);
+    setInterval(() => {
+        const previousImage = images[activeImage];
+        activeImage = (activeImage === (images.length - 1)) ? 0 : activeImage + 1;
+        slideImage(images[activeImage], previousImage);
+    }, 3000);
+}
+
+function slideImage(imageIn, imageOut) {
+    jQuery(imageOut).fadeOut(500, () => {
+        jQuery(imageIn).fadeIn(500);
+    });
+}
+
+/**
+ * RSVP
+ */
+jQuery('#rsvpForm input').keyup(function () {
+    var empty = false;
+    jQuery('form input').each(function () {
+        if (jQuery(this).val() === '') {
+            empty = true;
+        }
+    });
+
+    if (empty) {
+        jQuery('#rsvpButton').attr('disabled', 'disabled');
+    } else {
+        jQuery('#rsvpButton').removeAttr('disabled');
+    }
+});
+jQuery("#rsvpForm").submit(function (e) {
+    e.preventDefault();
 });
 
 function sendConfirmation(form) {
-  fields = form.elements;
-  const data = {};
-  jQuery("#rsvpForm :input").each(function(){
-    var input = $(this); // This is the jquery object of the input, do what you will
-       data[input[0].name] = input[0].value;
+    fields = form.elements;
+    const data = {};
+    jQuery("#rsvpForm :input").each(function () {
+        var input = $(this); // This is the jquery object of the input, do what you will
+        data[input[0].name] = input[0].value;
 
-       if(input[0].value === 'on') {
-        data[input[0].name] = true;
-       }
-   });
+        if (input[0].value === 'on') {
+            data[input[0].name] = true;
+        }
+    });
 
-  jQuery.post(
-    {
-      url: 'https://jessicaeroberto.it/administration/ag-wedding/confirmation',
-      headers: {
-        Authorization: 'Bearer cbb116f8b9708269d7e6339c13101d00d0caec84442e1e46e0861b2d61fef2b2'
-      },
-      contentType: "application/json",
-      dataType: 'json',
-      data: JSON.stringify(data)
-  })
-    .done(
-      (response)=>{console.log(response)}
-    );
+    jQuery.post(
+        {
+            url: 'https://jessicaeroberto.it/administration/ag-wedding/confirmation',
+            headers: {
+                Authorization: 'Bearer cbb116f8b9708269d7e6339c13101d00d0caec84442e1e46e0861b2d61fef2b2'
+            },
+            contentType: "application/json",
+            dataType: 'json',
+            data: JSON.stringify(data)
+        })
+        .done(
+            (response) => {
+                console.log(response);
+                // TODO: back to the welcome page
+            }
+        );
+}
+
+/**
+ * MOBILE SIDEBAR
+ */
+
+function toggleSidebar() {
+    if (jQuery('aside').hasClass('opened')) {
+        jQuery('#closeSidebarButton').hide();
+        jQuery('#sidebarToggleButton').show();
+        jQuery('aside').slideUp(200, () => {
+            jQuery('aside').removeClass('opened');
+        });
+    } else {
+        jQuery('#closeSidebarButton').show();
+        jQuery('#sidebarToggleButton').hide();
+
+        jQuery('aside').slideDown(200, () => {
+            jQuery('aside').addClass('opened');
+        });
+    }
 }
